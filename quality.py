73e3a1e82459095db19ef5d59fad40494f15eac5
Verprus <149952661+Verprus@users.noreply.github.com>
2024-06-28 13:20:25 +0200

    PRIM-75 Graf prikazuje vsako sekundo

--- quality.py
+++ quality.py
@@ -1,25 +1,29 @@
-import csv
 import numpy as np
 import matplotlib.pyplot as plt
+import csv
 
-file_path = "/Users/jureantolinc/Library/Mobile Documents/com~apple~CloudDocs/Revordings faks/untitled.csv"
+file_path = "/Users/jureantolinc/Library/Mobile Documents/com~apple~CloudDocs/Revordings faks/3/Gyro_AvtoCesta1_snemanje3.csv"
 
-# Open the CSV file and read the 'GyroscopeY' column into an array
 gyroscope_y_values = []
 with open(file_path, "r") as csv_file:
     csv_reader = csv.DictReader(csv_file, delimiter=';')
     for row in csv_reader:
         gyroscope_y_values.append(float(row['GyroscopeY']))
 
-# Convert the array to numpy array
 gyroscope_y_values = np.array(gyroscope_y_values)
 
-# Adjust time axis
 time = np.arange(0, len(gyroscope_y_values)) / 20.0  # assuming 20 Hz frequency
 
-# Plot the original data with positive and negative values
-plt.plot(time, np.abs(gyroscope_y_values))
-plt.xlabel('Time (s)')
-plt.ylabel('|GyroscopeY| Value')
-plt.title('Original GyroscopeY Data (Positive and Negative Values)')
+y_limit = max(abs(gyroscope_y_values) + 0.2)
+plt.ylim(-y_limit, y_limit)
+
+# Set x-axis ticks at each integer multiple of one second
+num_samples = len(time)
+num_seconds = num_samples // 20  # assuming 20 Hz frequency
+plt.xticks(np.arange(0, num_seconds + 1, step=1))
+
+plt.plot(time, gyroscope_y_values)
+plt.xlabel('Time (seconds)')
+plt.ylabel('GyroscopeY Value')
+plt.title('Original GyroscopeY Data')
 plt.show()
