dca80a8a97b071d4e6e4e7a856b978476516dcf1
Verprus <149952661+Verprus@users.noreply.github.com>
2024-06-28 13:24:31 +0200

    PRIM-75 demo ocenjevanje
    Dodano demo ocenjvanje skozi pobarvnje intervala v grafu na zeleno, rumeno in rdeco

--- quality.py
+++ quality.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.signal import find_peaks
 import csv
 
 file_path = "/Users/jureantolinc/Library/Mobile Documents/com~apple~CloudDocs/Revordings faks/3/Gyro_AvtoCesta1_snemanje3.csv"
@@ -26,4 +27,25 @@ plt.plot(time, gyroscope_y_values)
 plt.xlabel('Time (seconds)')
 plt.ylabel('GyroscopeY Value')
 plt.title('Original GyroscopeY Data')
+
+# Process each second interval
+for i in range(num_seconds):
+    start_idx = i * 20
+    end_idx = (i + 1) * 20
+    
+    # Find peaks within the interval
+    peaks, _ = find_peaks(np.abs(gyroscope_y_values[start_idx:end_idx]), height=3)
+    num_peaks = len(peaks)
+    
+    # Classify the interval based on the number of peaks
+    if any(np.abs(gyroscope_y_values[start_idx:end_idx]) > 3):
+        # If there are any peaks >3
+        plt.axvspan(i, i+1, color='red', alpha=0.3)
+    elif np.any(np.abs(gyroscope_y_values[start_idx:end_idx]) >= 2):
+        # If there's at least one peak with value 2 or higher
+        plt.axvspan(i, i+1, color='yellow', alpha=0.3)
+    else:
+        # Otherwise, it's green
+        plt.axvspan(i, i+1, color='green', alpha=0.3)
+
 plt.show()
